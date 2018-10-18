40de2c110e16333fc018648432c0915819640276
Tibo Keri <tiborkeri@icloud.com>
2018-10-18 16:43:09 +0100

    Inputfield / searchfield changes

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,25 +1,35 @@
 import React from 'react';
+import { InputGroup, InputGroupAddon, InputGroupText, Input, Form,FormGroup , Label} from 'reactstrap';
 import './style.css'
+
 
 const Search = () => {
 	return (
 		<div className="Searchfields">
-			<h2>Cash point locator</h2>
-			<div className="divider">
-				<form action="signup" submit='' className="form">
-					<label>All banks &nbsp;</label>
-					<input type="checkbox"/>
-					<label>Select a bank</label>
-					<select>
-						<option value="" selected disabled hidden>Select a bank</option>
-						<option value="">TSB</option>
-						<option value="">Barclays</option>
-						<option value="">Santanders</option>
-						<option value="">HSBC</option>
-					</select>
-				</form>
-			</div>
-	</div>
+			<Form>
+				<InputGroup>
+					<Input placeholder="ZIP code" />
+					<InputGroupAddon addonType="append">Search by ZIP code</InputGroupAddon>
+				</InputGroup>	
+				<FormGroup>
+          <Label for="exampleSelectMulti">Select Multiple</Label>
+          <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
+            <option>TSB</option>
+            <option>Barclays</option>
+            <option>Lloyds</option>
+            <option>HSBC</option>
+            <option>Natwide</option>
+          </Input>
+        </FormGroup>
+				<FormGroup check>
+							<Label check>
+								<Input type="radio" name="radio1" />{' '}
+								Wheelchair acces
+							</Label>
+				</FormGroup>
+			</Form>
+				
+		</div>
 	)	
 }
 
