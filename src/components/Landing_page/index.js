1c56574cae5cdc147a687814572c241c11d51a31
Tibo <tiborkeri@icloud.com>
2018-11-11 11:18:44 +0000

    Landing page component with bg image (- needs to change img quality)

--- src/components/Landing_page/index.js
+++ src/components/Landing_page/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Heading } from 'rebass';
-import { Hero, ScrollDownIndicator } from 'react-landing-page';
+import { Hero } from 'react-landing-page';
 import { Link } from 'react-router-dom';
 import './style.css';
 
 const Landing = () =>
   (
-    <Hero>
-      <Heading>Basic Hero</Heading>
-      <ScrollDownIndicator />
-      <Link to="/app">App</Link>
+    <Hero className="landing">
+      <Heading> <span>Bank</span>Locator</Heading>
+      <Link to="/app">Next</Link>
     </Hero>
   );
 export default Landing;
